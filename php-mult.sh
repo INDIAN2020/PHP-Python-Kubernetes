82548bef9b747f84a30018df302eadb8aa2a92be
gogula <gogula.sivannarayana@gmail.com>
2020-04-02 15:15:13 +0530

    Update php-mult.sh

--- php-mult.sh
+++ php-mult.sh
@@ -7,19 +7,17 @@
 #Author: Gogula <gogula.sivannarayana@gmail.com>
 #License: GPL3
 
-__install_PHP(){
-    if [[ "${PHP_VERSIONS}" != "" ]]; then
-    	# sudo apt install -y php-mysql php-fpm php-curl php-gd php-mbstring php-gettext php-token-stream php-gmagick php-tokenizer php-xdebug php-apcu php-memcache php-zip
-        for version in $PHP_VERSIONS; do
-			read -p "You need PHP ${version} ?" yn
-		    case $yn in
-		        [yY] | [yY][Ee][Ss] )
-		            echo -e "sudo apt install php$version php$version-fpm";
-					echo -e "sudo apt install -y php-gmagick php$version-xml php-ssh2 php$version-curl php$version-gd php$version-mbstring php$version-mysql php$version-bcmath php$version-bz2";
-		        ;;
-		        [nN] | [nN][Oo] ) ;;
-		        * ) echo "Please answer yes or no.";;
-		    esac
-		done
-    fi	
-}
+if [[ "${PHP_VERSIONS}" != "" ]]; then
+	# sudo apt install -y php-mysql php-fpm php-curl php-gd php-mbstring php-gettext php-token-stream php-gmagick php-tokenizer php-xdebug php-apcu php-memcache php-zip
+	for version in $PHP_VERSIONS; do
+		read -p "You need PHP ${version} ?" yn
+	    case $yn in
+		[yY] | [yY][Ee][Ss] )
+			echo -e "sudo apt install php$version php$version-fpm";
+			echo -e "sudo apt install -y php-gmagick php$version-xml php-ssh2 php$version-curl php$version-gd php$version-mbstring php$version-mysql php$version-bcmath php$version-bz2";
+		;;
+		[nN] | [nN][Oo] ) ;;
+		* ) echo "Please answer yes or no.";;
+	    esac
+	done
+fi	
