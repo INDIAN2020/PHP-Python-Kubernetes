4b04182357abfbf40a75c26fddd5c13bb4273390
Gogula <gogula.sivannarayana@gmail.com>
2020-04-28 18:25:26 +0530

    Update gcloud.sh

--- gcloud.sh
+++ gcloud.sh
@@ -11,7 +11,9 @@
 LATEST_URL=$(curl -sL https://cloud.google.com/sdk/docs/quickstart-linux | grep "linux-x86_64.tar.gz" | sed 's/.*href="//' | sed 's/".*//' | head -10);
 #Download the latest to temp folder
 sudo wget -c ${LATEST_URL} -O - | sudo tar -xz -C ${HOME}
-#Change permissions to 755
+#Change permissions to current user
+sudo chown $USER:$USER ${HOME}/google-cloud-sdk
+#change permissions to 755
 sudo chmod 755 ${HOME}/google-cloud-sdk -R
 #Add Cloud SDK tools to your path
 ./google-cloud-sdk/install.sh
