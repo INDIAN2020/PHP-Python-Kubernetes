e8e6738b0c47cf70549987a805537dabf027bc37
Gogula <gogula.sivannarayana@gmail.com>
2020-04-28 15:55:05 +0530

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -7,3 +7,9 @@
 #Author: Gogula <gogula.sivannarayana@gmail.com>
 #License: MIT
 
+#Download the latest release with the command:
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+#Make the kubectl binary executable
+chmod +x ./kubectl
+#Move the binary in to your PATH.
+sudo mv ./kubectl /usr/local/bin/kubectl
