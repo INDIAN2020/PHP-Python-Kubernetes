8ca8584fbc62bb0059959e5f75c42ef3e6b1feec
Gogula <gogula.sivannarayana@gmail.com>
2020-04-28 15:52:45 +0530

    Update gcloud.sh

--- gcloud.sh
+++ gcloud.sh
@@ -6,3 +6,10 @@
 #Brief: In development
 #Author: Gogula <gogula.sivannarayana@gmail.com>
 #License: MIT
+
+#Download the latest release with the command:
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+#Make the kubectl binary executable
+chmod +x ./kubectl
+#Move the binary in to your PATH.
+sudo mv ./kubectl /usr/local/bin/kubectl
